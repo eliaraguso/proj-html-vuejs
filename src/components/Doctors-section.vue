acfeae2dc0e8647edd412bc1da9ac6b133192af2
eliaraguso <elia.raguso@gmail.com>
2021-10-12 12:50:35 +0200

    Aggiornata doctors section (da completare a livello stilistico)

--- src/components/Doctors-section.vue
+++ src/components/Doctors-section.vue
@@ -14,7 +14,71 @@
     </div>
 
     <!-- Doctors section bottom -->
-    <div id="doctors-bottom"></div>
+    <div id="doctors-bottom">
+      <div class="doctor-box">
+        <img src="../assets/img/doctor-1.jpg" alt="doctor photo" />
+        <div class="doctor-box-bottom">
+          <div class="doctor-name">JON SNOW</div>
+          <div class="doctor-title">Anesthesiologist</div>
+          <div class="doctor-info">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque
+          </div>
+          <div class="doctor-social"></div>
+          <a href="#"
+            ><img src="../assets/img/facebook_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/instagram_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/twitter_icon.png" alt="facebook logo"
+          /></a>
+        </div>
+      </div>
+      <div class="doctor-box">
+        <img src="../assets/img/doctor-2.jpg" alt="doctor photo" />
+        <div class="doctor-box-bottom">
+          <div class="doctor-name">TONY STARK</div>
+          <div class="doctor-title">Cardiologist</div>
+          <div class="doctor-info">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque
+          </div>
+          <div class="doctor-social"></div>
+          <a href="#"
+            ><img src="../assets/img/facebook_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/instagram_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/twitter_icon.png" alt="facebook logo"
+          /></a>
+        </div>
+      </div>
+      <div class="doctor-box">
+        <img src="../assets/img/doctor-3.jpg" alt="doctor photo" />
+        <div class="doctor-box-bottom">
+          <div class="doctor-name">ANNA SMITH</div>
+          <div class="doctor-title">Nurse Practitioner</div>
+          <div class="doctor-info">
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque
+          </div>
+          <div class="doctor-social"></div>
+          <a href="#"
+            ><img src="../assets/img/facebook_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/instagram_icon.png" alt="facebook logo"
+          /></a>
+          <a href="#"
+            ><img src="../assets/img/twitter_icon.png" alt="facebook logo"
+          /></a>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -29,6 +93,8 @@ export default {
 #doctors-section {
   background-color: #f7f7f7;
 }
+
+// Doctors top section
 #doctors-top {
   border: 5px solid brown;
   width: 33%;
@@ -43,11 +109,40 @@ export default {
   h2 {
     font-size: 46px;
     font-weight: 300;
+    padding-top: 20px;
   }
   h3 {
     font-size: 22px;
     font-weight: 300;
     line-height: 48px;
+  }
+}
+
+// Doctors bottom section
+#doctors-bottom {
+  width: 53%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  .doctor-box {
+    border: 5px solid green;
+    width: 369px;
+    font-size: 18px;
+    font-weight: 300;
+    .doctor-box-bottom {
+      padding: 35px;
+      line-height: 38px;
+      color: #333c4e;
+      background-color: white;
+      .doctor-info {
+        margin: 30px 0;
+        color: #7e8890;
+      }
+      img {
+        width: 35px;
+        margin-right: 15px;
+      }
+    }
   }
 }
 </style>
