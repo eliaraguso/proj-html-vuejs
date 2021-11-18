27b6edc84c88b8148137ff6fa4a570bd626034f5
eliaraguso <elia.raguso@gmail.com>
2021-11-18 14:08:01 +0100

    seconda prova deploy su github pages

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:eliaraguso/proj-html-vuejs.git main:gh-pages
+git push -f git@github.com:eliaraguso/proj-html-vuejs.git master:gh-pages
 
 cd -
